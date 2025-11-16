9ec570618f79d3857f337751024e942f182b0f47
martinportin <martinportin@gmail.com>
2025-11-16 18:10:04 +0200

    Failed test

--- src/routes/page.svelte.spec.ts
+++ src/routes/page.svelte.spec.ts
@@ -4,10 +4,10 @@ import { render } from 'vitest-browser-svelte';
 import Page from './+page.svelte';
 
 describe('/+page.svelte', () => {
-	it('should render h1', async () => {
+	it('should render a paragraph', async () => {
 		render(Page);
 
-		const heading = page.getByRole('heading', { level: 1 });
-		await expect.element(heading).toBeInTheDocument();
+		const paragraph = page.getByText(/daily-wittgensten/i);
+		await expect.element(paragraph).toBeInTheDocument();
 	});
 });
