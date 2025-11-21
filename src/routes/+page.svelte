032a7588d1d3d41858eb1c219f494d57181b27ad
martinportin <martinportin@gmail.com>
2025-11-21 22:02:30 +0200

    Added Paragraph component to first page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<p>daily-wittgenstein</p>
+<script lang="ts">
+	import Paragraph from './components/Paragraph.svelte';
+
+	interface Props {
+		number: string;
+		quote: string;
+		source: string;
+	}
+
+	let { number, quote, source }: Props = $props();
+</script>
+
+<Paragraph {number} {quote} {source} />
--- src/routes/page.svelte.spec.ts
+++ src/routes/page.svelte.spec.ts
@@ -1,11 +1,11 @@
+import { page } from 'vitest/browser';
 import { describe, expect, test } from 'vitest';
 import { render } from 'vitest-browser-svelte';
-import Paragraph from './Paragraph.svelte';
-import { page } from 'vitest/browser';
+import Page from './+page.svelte';
 
-describe('Paragraph Component', () => {
-	test('should display "1", "first quote" and "source one', () => {
-		render(Paragraph, { number: '1', quote: 'first quote', source: 'source one' });
+describe('/+page.svelte', () => {
+	test('should render "1", "first quote" and "source one"', async () => {
+		render(Page, { number: '1', quote: 'first quote', source: 'source one' });
 
 		const number = page.getByText(/1/i);
 		const quote = page.getByText(/first quote/i);
@@ -16,8 +16,8 @@ describe('Paragraph Component', () => {
 		expect(source).toBeInTheDocument();
 	});
 
-	test('should display "2", "second quote" and "source two"', () => {
-		render(Paragraph, { number: '2', quote: 'second quote', source: 'source two' });
+	test('should render "2", "second quote" and "source two"', () => {
+		render(Page, { number: '2', quote: 'second quote', source: 'source two' });
 
 		const number = page.getByText(/2/i);
 		const quote = page.getByText(/second quote/i);
@@ -28,8 +28,8 @@ describe('Paragraph Component', () => {
 		expect(source).toBeInTheDocument();
 	});
 
-	test('should display "3", "third quote" and "source three"', () => {
-		render(Paragraph, { number: '3', quote: 'third quote', source: 'source three' });
+	test('should render "3", "third quote" and "source third"', () => {
+		render(Page, { number: '3', quote: 'third quote', source: 'source three' });
 
 		const number = page.getByText(/3/i);
 		const quote = page.getByText(/third quote/i);
